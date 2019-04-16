d6df58dfe443eb1d344e13b5a608e370decc6a93
dellpooh47 <shoboon47@gmail.com>
2019-04-17 00:28:41 +0900

    占いファイル追加

---
+++ js/sukikirai1.js
@@ -0,0 +1,9 @@
+// if文による条件分岐
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var number = 1;
+if (number == 1) {
+    console.log("スキ！"); // 条件式が成立したときの処理
+} else {
+    console.log("キライ");  // 条件式が成立しなかったときの処理
+}
